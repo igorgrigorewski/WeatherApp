904efd7a982b606bdff3d13c97ad316ee5b23712
igorgrigorewski <igorgrigorewski@gmail.com>
2017-05-13 10:46:18 +0300

    Application using GPS  to get weather in your location.

--- app/src/main/java/com/igorgrigorewski/weatherapp/api/ApiService.java
+++ app/src/main/java/com/igorgrigorewski/weatherapp/api/ApiService.java
@@ -4,8 +4,9 @@ import com.igorgrigorewski.weatherapp.content.CurrentWeatherData;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Url;
 
 public interface ApiService {
-    @GET("/data/2.5/weather?q=Odessa&units=metric&appid=501001a080ff4c6d00cbf90caf41f64d")
-    Call<CurrentWeatherData> getWeather();
+    @GET
+    Call<CurrentWeatherData> getWeather(@Url String location);
 }
--- app/src/main/java/com/igorgrigorewski/weatherapp/ui/MainActivity.java
+++ app/src/main/java/com/igorgrigorewski/weatherapp/ui/MainActivity.java
@@ -1,13 +1,18 @@
 package com.igorgrigorewski.weatherapp.ui;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.igorgrigorewski.weatherapp.R;
-import com.igorgrigorewski.weatherapp.WeatherController;
-import com.igorgrigorewski.weatherapp.api.ApiService;
 import com.igorgrigorewski.weatherapp.api.RetroClient;
 import com.igorgrigorewski.weatherapp.content.CurrentWeatherData;
 
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView TemperatureTextView;
     private TextView CityTextView;
 
+    private Location lastKnownLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +35,57 @@ public class MainActivity extends AppCompatActivity {
         TemperatureTextView = (TextView) findViewById(R.id.temperature);
         CityTextView = (TextView) findViewById(R.id.city);
 
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(isBetterLocation(location, lastKnownLocation)){
+                    lastKnownLocation = location;
+                    onWeatherUpdate();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+
         onWeatherUpdate();
     }
 
-    private void onWeatherUpdate(){
-        RetroClient.getApiService().getWeather().enqueue(new Callback<CurrentWeatherData>() {
+
+    private void onWeatherUpdate() {
+        String url = "/data/2.5/weather" +
+                "?lat=" + lastKnownLocation.getLatitude() +
+                "&lon="+ lastKnownLocation.getLongitude() +
+                "&units=metric" +
+                "&appid=501001a080ff4c6d00cbf90caf41f64d";
+        RetroClient.getApiService().getWeather(url).enqueue(new Callback<CurrentWeatherData>() {
             @Override
             public void onResponse(Call<CurrentWeatherData> call, Response<CurrentWeatherData> response) {
                 if (response.isSuccessful()){
@@ -47,5 +100,61 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("ERROR", "api response isn't successful");
             }
         });
+    }
+
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 }
